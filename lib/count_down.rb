de4cbb7c4f7711930877b2639ff458d1d995ee99
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-05-19 23:37:49 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,7 @@
 # Write your code here
 starting_number = 10
-magic_exit_number = 1
-while starting_number >=1 do
+magic_exit_number = 0
+while starting_number >=0 do
   break if starting_number == magic_exit_number
   puts starting_number
   starting_number -= 1 
