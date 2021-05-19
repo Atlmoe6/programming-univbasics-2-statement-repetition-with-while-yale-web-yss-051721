1089db32d82914e5e72484df87d252aabe005061
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-05-19 23:34:26 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,9 @@
 # Write your code here
 starting_number = 10
-while starting_number >= 1 do
+magic_exit_number = 1
+while starting_number > 0 do
+  break if starting_number == magic_exit_number
   puts starting_number
-  starting_number -= 10 
+  starting_number -= 1
 end
 puts "Happy New Year!"
